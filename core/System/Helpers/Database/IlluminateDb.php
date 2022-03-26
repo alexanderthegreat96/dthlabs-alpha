50e8275eeecc2533fa3218708f8b3f8cf87c9806
Alexandru Lupaescu <alexandrulupaescu@gmail.com>
2022-03-26 11:19:40 -0700

    Another fix.

--- core/System/Helpers/Database/IlluminateDb.php
+++ core/System/Helpers/Database/IlluminateDb.php
@@ -1,11 +1,53 @@
 <?php
+
 namespace LexSystems\Core\System\Helpers\Database;
+
 use Illuminate\Container\Container;
+use LexSystems\Core\System\Helpers\Arrays\ArrayUtility;
+use LexSystems\Core\System\Helpers\CoreUtils\SqlFormatter;
+use LexSystems\Core\System\Helpers\Debugger;
 
 class IlluminateDb extends \Illuminate\Database\Capsule\Manager
 {
+    /**
+     * @param Container|null $container
+     */
     public function __construct(Container $container = null)
     {
         parent::__construct($container);
+    }
+
+    /**
+     * Return SQLFormatter::format
+     */
+    public static function getQuery()
+    {
+        $queries = IlluminateDb::connection()->getQueryLog();
+        $formattedQueries = [];
+        foreach ($queries as $query) :
+            $prep = $query['query'];
+
+            foreach ($query['bindings'] as $binding) :
+
+                if (is_bool($binding)) {
+                    $val = $binding === true ? 'TRUE' : 'FALSE';
+                } else if (is_numeric($binding)) {
+                    $val = $binding;
+                } else {
+                    $val = "'$binding'";
+                }
+
+                $prep = preg_replace("#\?#", $val, $prep, 1);
+            endforeach;
+            $formattedQueries[] = $prep;
+        endforeach;
+
+        if ($formattedQueries) {
+            echo '<ul>';
+            foreach ($formattedQueries as $query) {
+                echo '<li>' . SqlFormatter::format($query) . '</li>';
+            }
+            echo '</ul>';
+        }
     }
 }
